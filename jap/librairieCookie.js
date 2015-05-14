e35798507e5e07ea8fddba3809537b827e3f7dea
GabrielFleury <gabriel.fleury1@uqac.ca>
2015-05-13 22:57:07 -0400

    katakana v0.1
    katakana practice, doesnt check for duplicates yet

---
+++ jap/librairieCookie.js
@@ -0,0 +1,52 @@
+﻿/*
+*  ecrireCookie()
+*                 fonction pour écrire un cookie sur l'ordinateur du client
+*  @return: null;
+*  @parameter:  pNom         ,le nom du cookie 
+*  @parameter:  pValeur      ,la valeur de ce cookie
+*  @parameter:  pJours       ,le nombre de jours que sera sauvegardé ce cookie
+*/
+function ecrireCookie(pNom, pValeur, pJours) {
+  // Par défaut, le cookie est temporaire, sans date d’expiration
+  var expires = "";
+
+  // Spécifie le nombre de jours que le cookie persistera
+  if (pJours) {
+    var date = new Date();
+    date.setTime(date.getTime() + (pJours * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toGMTString();
+  }
+
+  // Crée le cookie avec le nom, sa valeur et la date d’expiration
+  document.cookie = pNom + "=" + pValeur + expires + "; path=/";
+}
+/*
+*  lireCookie()
+*              fonction pour lire un cookie sur l'ordinateur du client
+*  @return: null;
+*  @prameter: pNom,  le nom du cookie 
+*/
+function lireCookie(pNom) {
+  // Trouve le cookie demandé et retour sa valeur
+  var searchpNom = pNom + "=";
+  var cookies = document.cookie.split(';');
+  for(var i=0; i < cookies.length; i++) {
+    var c = cookies[i];
+    while (c.charAt(0) == ' ')
+      c = c.substring(1, c.length);
+    if (c.indexOf(searchpNom) == 0)
+      return c.substring(searchpNom.length, c.length);
+  }
+  return null;
+}
+/*
+*  effacerCookie()
+*              fonction pour effacer un cookie sur l'ordinateur du client avant sa date d'expliration
+*  @return: null;
+*  @prameter: pNom,  le nom du cookie 
+*/
+function effacerCookie(pNom) {
+  // Efface le cookie en écrivant un nouveau au même nom, 
+  // sans valeur et en demande une date d’expiration dans le passé
+  ecrireCookie(pNom, "", -1);
+}
---
+++ jap/script.js
@@ -0,0 +1,228 @@
+﻿/**
+ * @author Gabriel Fleury
+ */
+ 
+$(document).ready(initialiser);	
+$pts=0;
+$ptsTotal=0;
+
+function initialiser() 
+{
+	reroll("a");
+	
+	//permettre le click
+	$(".answer").click(verifierReponse);
+}
+
+function reroll($derniereQuestion){
+	//générer une question random
+	$randomNum = Math.floor((Math.random() * 79));
+	$randomQuestion = hiraB[$randomNum];
+	while($randomQuestion=="nothing" || $randomQuestion==$derniereQuestion)
+	{
+		$randomNum = Math.floor((Math.random() * 79));
+		$randomQuestion = hiraB[$randomNum];
+	}
+	$("#question").text($randomQuestion);
+	
+	//générer des réponses random
+	$(".answer").each(function( i ) {
+		$(this).text(creerRandomReponse());
+	});
+	
+	//insérer la bonne réponse dans une case random
+	$(".answer").eq( Math.floor((Math.random()*9)) ).text( hiraA[$randomNum] );
+}
+
+function creerRandomReponse(){
+	$randomReponse = hiraA[Math.floor((Math.random() * 79))];
+	while($randomReponse=="nothing")
+	{
+		$randomReponse = hiraA[Math.floor((Math.random() * 79))];
+	}
+	return $randomReponse;
+}
+
+function verifierReponse() 
+{
+	//vérifier
+	$question = $("#question").text();
+	$reponse = $(this).text();
+	$questionNum = $.inArray($question, hiraB);
+	$reponseNum = $.inArray($reponse, hiraA);
+	
+	//update pointage
+	if($questionNum==$reponseNum){
+		$pts++;
+	}
+	$ptsTotal++;
+	$("#score").text("Score: " + $pts + "/" + $ptsTotal);
+	
+	reroll($question);
+}
+
+var hiraA = [
+    "あ",
+    "い",
+    "う",
+    "え",
+    "お",
+    "か",
+    "き",
+    "く",
+    "け",
+    "こ",
+    "さ",
+    "し",
+    "す",
+    "せ",
+    "そ",
+    "た",
+    "ち",
+    "つ",
+    "て",
+    "と",
+    "な",
+    "に",
+    "ぬ",
+    "ね",
+    "の",
+    "は",
+    "ひ",
+    "ふ",
+    "へ",
+    "ほ",
+    "ま",
+    "み",
+    "む",
+    "め",
+    "も",
+    "や",
+    "nothing",
+    "ゆ",
+    "nothing",
+    "よ",
+    "ら",
+    "り",
+    "る",
+    "れ",
+    "ろ",
+    "わ",
+    "nothing",
+    "nothing",
+    "nothing",
+    "を",
+	"nothing",
+    "nothing",
+	"nothing",
+    "nothing",
+	"ん",
+    "が",
+	"ぎ",
+    "ぐ",
+	"げ",
+    "ご",
+	"ざ",
+	"じ",
+    "ず",
+	"ぜ",
+    "ぞ",
+	"だ",
+	"nothing",
+    "nothing",
+	"で",
+    "ど",
+	"ば",
+	"び",
+    "ぶ",
+	"べ",
+    "ぼ",
+	"ぱ",
+	"ぴ",
+    "ぷ",
+	"ぺ",
+    "ぽ"
+];
+
+var hiraB = [
+    "a",
+    "i",
+    "u",
+    "e",
+    "o",
+    "ka",
+    "ki",
+    "ku",
+    "ke",
+    "ko",
+    "sa",
+    "shi",
+    "su",
+    "se",
+    "so",
+    "ta",
+    "chi",
+    "tsu",
+    "te",
+    "to",
+    "na",
+    "ni",
+    "nu",
+    "ne",
+    "no",
+    "ha",
+    "hi",
+    "fu",
+    "he",
+    "ho",
+    "ma",
+    "mi",
+    "mu",
+    "me",
+    "mo",
+    "ya",
+    "nothing",
+    "yu",
+    "nothing",
+    "yo",
+    "ra",
+    "ri",
+    "ru",
+    "re",
+    "ro",
+    "wa",
+    "nothing",
+    "nothing",
+    "nothing",
+    "wo",
+	"nothing",
+    "nothing",
+	"nothing",
+    "nothing",
+	"n",
+    "ga",
+	"gi",
+    "gu",
+	"ge",
+    "go",
+	"za",
+	"ji",
+    "zu",
+	"ze",
+    "zo",
+	"da",
+	"nothing",
+    "nothing",
+	"de",
+    "do",
+	"ba",
+	"bi",
+    "bu",
+	"be",
+    "bo",
+	"pa",
+	"pi",
+    "pu",
+	"pe",
+    "po"
+];
